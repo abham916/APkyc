4801bb0e4282f6a6585e41767a9266ee4f79a538
Young Chan Kim <abham916@gmail.com>
2019-03-12 08:39:15 +0800

    code fixed

--- 3.1.4.kt
+++ 3.1.4.kt
@@ -2,17 +2,20 @@
  * Date: 2019.03.08.
  * Goal: Print *****
 				****
-				 ***
-				  **
-				   *
+				 **
+				  *
  */
  
 fun main(){
+	var space = 1
+	
 	for(i in 1..5){
-		for(z in 1..i-1)
+		if(i == 3) continue
+		for(z in 1..space-1)
 			print(" ")
 		for(j in i..5)
 			print("*")
 		println()
+		space++
 	}
 }
